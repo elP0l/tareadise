51a1e6e2327664facb23f5f013ecd6713430f6e4
paul123 <paulalcivar2004@gmail.com>
2024-08-15 22:43:13 -0500

    avance2

--- tallerpruebas/tallerpruebas/EmployeeTest.java
+++ tallerpruebas/tallerpruebas/EmployeeTest.java
@@ -13,10 +13,10 @@ public class EmployeeTest  {
 	@Before
     public void antesdeTest() {
 		workerUSD = new Employee(1200, "USD", 0, EmployeeType.Worker);
-        supervisorUSD = new Employee(1500, "USD", 10, EmployeeType.Supervisor);
-        supervisoOtherCurrency = new Employee(1500, "Bolivar", 10, EmployeeType.Supervisor);
-        managerUSD=new Employee(2000, "USD", 20, EmployeeType.Manager);
-        managerOtherCurrency = new Employee(2000, "EUR", 20, EmployeeType.Manager);
+        supervisorUSD = new Employee(1500, "USD", 0.10F, EmployeeType.Supervisor);
+        supervisoOtherCurrency = new Employee(1500, "Bolivar", 0.10F, EmployeeType.Supervisor);
+        managerUSD=new Employee(2000, "USD", 0.20F, EmployeeType.Manager);
+        managerOtherCurrency = new Employee(2000, "EUR",0.20F, EmployeeType.Manager);
     }
 	@Test
 	public void testCsWorkerUSDImpar() {
@@ -40,28 +40,66 @@ public class EmployeeTest  {
     }
 	@Test
 	public void testCsSupervisorUSDPar() {
-		float expected = 1500 + 10 * 0.35F;
-		assertEquals(expected, supervisorUSD.cs(), 0.01);
-	    }
+	    float salarioAjustado = 1500; 
+	    float bono = 10 * 0.35F; 
+	    float expected = salarioAjustado + bono; 
+	    assertEquals(expected, supervisorUSD.cs(), 0.01);
+	}
 	@Test
-    public void testCsSupervisorUSDImpar() {
-        float expected = 1500 + 10 * 0.35F + 386 / 12 * 2;
-        assertEquals(expected, supervisorUSD.cs(), 0.01);
-    }
+	public void testCsSupervisorUSDImpar() {
+	    float salarioAjustado = 1500; 
+	    float bono = 10 * 0.35F; 
+	    float rmuPorMes = 386.0F / 12 * 2; 
+	    float expected = salarioAjustado + bono + rmuPorMes;
+	    assertEquals(expected, supervisorUSD.cs(), 0.01);
+	}
 	@Test
     public void testCsSupervisorOtherCurrencyImpar() {
         float salarioAjustado = 1500 * 0.95F; 
         float expected = salarioAjustado + 10 * 0.35F + 386 / 12 * 2;
         assertEquals(expected, supervisoOtherCurrency.cs(), 0.01);
     }
-
     @Test
     public void testCsSupervisorOtherCurrencyPar() {
         float salarioAjustado = 1500 * 0.95F; // 
         float expected = salarioAjustado + 10 * 0.35F;
         assertEquals(expected, supervisoOtherCurrency.cs(), 0.01);
     }
+    @Test
+    public void testCalculateYearBonusWorkerUSD() {
+        float expected = 386.0F;
+        assertEquals(expected, workerUSD.CalculateYearBonus(), 0.01);
+    }
+    @Test
+    public void testCalculateYearBonusSupervisorUSD() {
+        float expected = 1500 + 386.0F * 0.5F;
+        assertEquals(expected, supervisorUSD.CalculateYearBonus(), 0.01);
+    }
+
+    @Test
+    public void testCalculateYearBonusManagerUSD() {
+        float expected = 2000 + 386.0F * 1.0F; 
+        assertEquals(expected, managerUSD.CalculateYearBonus(), 0.01);
+    }
+    @Test
+    public void testCalculateYearBonusWorkerOtherCurrency() {
+        float expected = 386.0F * 0.95F;
+        assertEquals(expected, supervisoOtherCurrency.CalculateYearBonus(), 0.01);
+    }
+    @Test
+    public void testCalculateYearBonusSupervisorOtherCurrency() {
+        float salarioAjustado = 1500 * 0.95F;
+        float expected = salarioAjustado + 386.0F * 0.5F; 
+        assertEquals(expected, supervisoOtherCurrency.CalculateYearBonus(), 0.01);
+    }
+    @Test
+    public void testCalculateYearBonusManagerOtherCurrency() {
+        float salarioAjustado = 2000 * 0.95F;
+        float expected = salarioAjustado + 386.0F * 1.0F; 
+        assertEquals(expected, managerOtherCurrency.CalculateYearBonus(), 0.01);
+    }
 }
+
 	
 	
 	
