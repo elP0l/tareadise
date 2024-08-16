4b07dd7f8ab29dca0479031be4882696838bea03
JoseM0lina <josmmoli@espol.edu.ec>
2024-08-15 21:45:11 -0500

    Avance

--- tallerpruebas/tallerpruebas/EmployeeTest.java
+++ tallerpruebas/tallerpruebas/EmployeeTest.java
@@ -1,15 +1,15 @@
 package tallerpruebas;
+import junit.framework.TestCase;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
-import org.junit.BeforeClass;
+import org.junit.Before;
 
-
-public class EmployeeTest {
+public class EmployeeTest extends TestCase {
 	private Employee workerUSD;
 	private Employee  supervisorUSD;
 	private Employee  managerOtherCurrency;
 	
-	@BeforeClass
+	@Before
     public void antesdeTest() {
 		workerUSD = new Employee(1200, "USD", 0, EmployeeType.Worker);
         supervisorUSD = new Employee(1500, "USD", 10, EmployeeType.Supervisor);
@@ -19,6 +19,21 @@ public class EmployeeTest {
 	public void testCsWorkerUSDImpar() {
 		float expected = 1200 + 386/12*2;
 		assertEquals(expected, workerUSD.cs(), 0.01);
-	    }
+	}
+	@Test
+	public void testCsWorkerUSDPar() {
+		float expected = 1200;
+		assertEquals(expected, workerUSD.cs(), 0.01);
+	}
+	@Test
+    public void testCsWorkerOtherCurrencyPar() {
+        float expected = (float) (1200 * 0.95);
+        assertEquals(expected, managerOtherCurrency.cs(), 0.01);
+    }
+	@Test
+    public void testCsWorkerOtherCurrencyImpar() {
+        float expected = (float) ((1200 * 0.95) + 386/12*2);
+        assertEquals(expected, managerOtherCurrency.cs(), 0.01);
+    }
 	
 }
