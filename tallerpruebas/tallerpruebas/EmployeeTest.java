d908bc5dd8635d278ff471b83ee096e1349f7b47
paul123 <paulalcivar2004@gmail.com>
2024-08-15 22:36:49 -0500

    avance

--- tallerpruebas/tallerpruebas/EmployeeTest.java
+++ tallerpruebas/tallerpruebas/EmployeeTest.java
@@ -1,18 +1,21 @@
 package tallerpruebas;
-import junit.framework.TestCase;
 import static org.junit.Assert.assertEquals;
 import org.junit.Test;
 import org.junit.Before;
 
-public class EmployeeTest extends TestCase {
+public class EmployeeTest  {
 	private Employee workerUSD;
 	private Employee  supervisorUSD;
 	private Employee  managerOtherCurrency;
+	private Employee supervisoOtherCurrency;
+	private Employee managerUSD;
 	
 	@Before
     public void antesdeTest() {
 		workerUSD = new Employee(1200, "USD", 0, EmployeeType.Worker);
         supervisorUSD = new Employee(1500, "USD", 10, EmployeeType.Supervisor);
+        supervisoOtherCurrency = new Employee(1500, "Bolivar", 10, EmployeeType.Supervisor);
+        managerUSD=new Employee(2000, "USD", 20, EmployeeType.Manager);
         managerOtherCurrency = new Employee(2000, "EUR", 20, EmployeeType.Manager);
     }
 	@Test
@@ -35,5 +38,31 @@ public class EmployeeTest extends TestCase {
         float expected = (float) ((1200 * 0.95) + 386/12*2);
         assertEquals(expected, managerOtherCurrency.cs(), 0.01);
     }
-	
+	@Test
+	public void testCsSupervisorUSDPar() {
+		float expected = 1500 + 10 * 0.35F;
+		assertEquals(expected, supervisorUSD.cs(), 0.01);
+	    }
+	@Test
+    public void testCsSupervisorUSDImpar() {
+        float expected = 1500 + 10 * 0.35F + 386 / 12 * 2;
+        assertEquals(expected, supervisorUSD.cs(), 0.01);
+    }
+	@Test
+    public void testCsSupervisorOtherCurrencyImpar() {
+        float salarioAjustado = 1500 * 0.95F; 
+        float expected = salarioAjustado + 10 * 0.35F + 386 / 12 * 2;
+        assertEquals(expected, supervisoOtherCurrency.cs(), 0.01);
+    }
+
+    @Test
+    public void testCsSupervisorOtherCurrencyPar() {
+        float salarioAjustado = 1500 * 0.95F; // 
+        float expected = salarioAjustado + 10 * 0.35F;
+        assertEquals(expected, supervisoOtherCurrency.cs(), 0.01);
+    }
 }
+	
+	
+	
+
